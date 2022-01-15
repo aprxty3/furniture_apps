cdd7fc39cf23830980244d5d73e1a89263f7d372
aprxty3 <ajiprxty3@gmail.com>
2022-01-15 16:38:05 +0700

    add style theme

---
+++ lib/theme.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kBlack = const Color(0xff222222);
+Color kBlackAccent = const Color(0xff2A2B37);
+Color kGrey = const Color(0xff94959B);
+Color kLineDark = const Color(0xffEAEAEA);
+Color kWhiteGrey = const Color(0xffF1F1F5);
+Color kWhite = const Color(0xffFFFFFF);
+Color kRed = const Color(0xffFF314A);
+Color kYellow = const Color(0xffFFBA00);
+Color kBlue = const Color(0xff2972FF);
+
+TextStyle splashStyle = GoogleFonts.nunito(
+  fontSize: 36,
+  fontWeight: FontWeight.w700,
+  color: kWhite,
+);
+
+//ONBOARDING
+TextStyle onboard1 = GoogleFonts.nunito(
+  fontSize: 26,
+  fontWeight: FontWeight.w400,
+  color: kBlack,
+);
+
+TextStyle onboard2 = GoogleFonts.nunito(
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+  color: kGrey,
+);
+
+TextStyle onboard3 = GoogleFonts.nunito(
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+  color: kBlack,
+);
+
+//SIGN IN
+TextStyle mainStyle = GoogleFonts.nunito(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  color: kBlack,
+);
+
+TextStyle rememberMe = GoogleFonts.nunito(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  color: kBlack,
+);
+
+TextStyle loginGoogle = GoogleFonts.nunito(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  color: kBlack,
+);
+
+//HOMEPAGE
+TextStyle spaceStyle = GoogleFonts.roboto(
+  fontSize: 19.5,
+  fontWeight: FontWeight.w700,
+  color: kBlack,
+);
+
+TextStyle showAllStyle = GoogleFonts.nunito(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  color: kBlack,
+);
+
+TextStyle categoryStyle1 = GoogleFonts.nunito(
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+  color: kBlack,
+);
+
+TextStyle categoryStyle2 = GoogleFonts.nunito(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  color: kBlack,
+);
+
+TextStyle categoryStyle3 = GoogleFonts.nunito(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  color: kGrey,
+);
+
+TextStyle categoryStyle4 = GoogleFonts.nunito(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  color: kGrey,
+);
+
+TextStyle popularStyle1 = GoogleFonts.nunito(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+  color: kBlack,
+);
+
+TextStyle popularStyle2 = GoogleFonts.nunito(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  color: kBlack,
+);
+
+//RECOMMENDATION
+
+TextStyle recomStyle = GoogleFonts.nunito(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  color: kBlack,
+);
+
+//PROFILE
+
+TextStyle profileStyle1 = GoogleFonts.nunito(
+  fontSize: 24,
+  fontWeight: FontWeight.w500,
+  color: const Color(0xff18192B),
+);
+
+TextStyle profileStyle2 = GoogleFonts.nunito(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  color: const Color(0xff18192B),
+);
