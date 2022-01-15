50810ce7bd5cba818e4a8b86a2ab0c0a6ff3d090
aprxty3 <ajiprxty3@gmail.com>
2022-01-15 21:35:19 +0700

    build onboard page

--- lib/pages/onboarding_page.dart
+++ lib/pages/onboarding_page.dart
@@ -1,1 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:furniture_apps/theme.dart';
+
+class OnboardPage extends StatelessWidget {
+  const OnboardPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 99, bottom: 127),
+              width: double.infinity,
+              height: 322,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/onboard1.png'),
+                ),
+              ),
+            ),
+            Text(
+              'Buy Furniture Easily',
+              style: onboard1,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Aliqua id fugiat nostrud irure ex duis ea\nquis id quis ad et. Sunt qui esse',
+              style: onboard2,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
