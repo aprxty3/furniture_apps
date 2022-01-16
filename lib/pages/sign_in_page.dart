ac19034b73ad94fb0c4323684a1b3a1f94819435
aprxty3 <ajiprxty3@gmail.com>
2022-01-16 14:24:46 +0700

    build SignIn page

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,10 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:furniture_apps/theme.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
 
   @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          children: [
+            title(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget title() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Login to your\naccount',
+            style: mainStyle,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Container(
+                width: 87,
+                height: 4,
+                decoration: BoxDecoration(
+                  color: kBlack,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+              const SizedBox(
+                width: 4,
+              ),
+              Container(
+                width: 8,
+                height: 4,
+                decoration: BoxDecoration(
+                  color: kBlack,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget email() {
+    return Container(
+      margin: const EdgeInsets.only(top: 48),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(14),
+        color: kWhiteGrey,
+      ),
+      child: TextFormField(
+        decoration: InputDecoration.collapsed(
+          hintText: 'Email',
+          hintStyle: hintTextStyle,
+        ),
+      ),
+    );
   }
 }
