e25a9187523cdc9cccea2928adee5f45dd44f8be
aprxty3 <ajiprxty3@gmail.com>
2022-01-20 21:45:03 +0700

    created search field on appbar

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -8,34 +8,49 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kWhiteGrey,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: kWhite,
-        elevation: 0,
-        title: Row(
-          children: [
-            Icon(
-              Icons.chevron_left,
-              color: kBlack,
-            ),
-            const SizedBox(
-              width: 18,
-            ),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: kWhiteGrey,
-                  borderRadius: BorderRadius.circular(14),
-                ),
-                child: Row(
-                  children: [
-                    TextFormField(),
-                  ],
+      appBar: PreferredSize(
+        preferredSize: const Size(double.infinity, 70),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: kWhite,
+          elevation: 0,
+          title: Row(
+            children: [
+              Icon(
+                Icons.chevron_left,
+                color: kBlack,
+              ),
+              const SizedBox(
+                width: 18,
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: kWhiteGrey,
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration.collapsed(
+                              hintText: 'Search Furniture'),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Icon(
+                        Icons.close,
+                        color: kGrey,
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       body: Center(
