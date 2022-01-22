16fa3eb82702298b76eb74b3b8f33c790fdb983f
aprxty3 <ajiprxty3@gmail.com>
2022-01-23 00:26:43 +0700

    add Navigator to Category page

--- lib/widgets/category_cart_widget.dart
+++ lib/widgets/category_cart_widget.dart
@@ -15,51 +15,56 @@ class CategoryCartWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(0),
-      height: 123,
-      width: MediaQuery.of(context).size.width - (2 * 24),
-      child: Stack(
-        children: [
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: 102,
-              decoration: BoxDecoration(
-                color: kWhite,
-                borderRadius: BorderRadius.circular(14),
-              ),
-              child: Row(
-                children: [
-                  const SizedBox(
-                    width: 150,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        mainText,
-                        style: categoryStyle2,
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        subText,
-                        style: categoryStyle3,
-                      ),
-                    ],
-                  ),
-                ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/category');
+      },
+      child: Container(
+        margin: const EdgeInsets.all(0),
+        height: 123,
+        width: MediaQuery.of(context).size.width - (2 * 24),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 102,
+                decoration: BoxDecoration(
+                  color: kWhite,
+                  borderRadius: BorderRadius.circular(14),
+                ),
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 150,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          mainText,
+                          style: categoryStyle2,
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          subText,
+                          style: categoryStyle3,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Image.asset(
-            imageUrl,
-            height: 122,
-          )
-        ],
+            Image.asset(
+              imageUrl,
+              height: 122,
+            )
+          ],
+        ),
       ),
     );
   }
