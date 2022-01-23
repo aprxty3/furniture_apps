a927b45d88a12b367cd7f5c310d2667a96dc456a
aprxrt3 <ajiprxty3@gmail.com>
2022-01-23 23:47:48 +0700

    add bottom navbar

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
 
+import '../theme.dart';
+
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BottomNavigationBar(
+          showUnselectedLabels: false,
+          showSelectedLabels: false,
+          backgroundColor: kWhite,
+          onTap: (value) {
+            if (value == 0) {
+              Navigator.pushNamed(context, '/home');
+            }
+            if (value == 1) {
+              Navigator.pushNamed(context, '/wishlist');
+            }
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icon_home.png',
+                width: 24,
+              ),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icon_heart.png',
+                width: 24,
+              ),
+              label: 'wishlish',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icon_person.png',
+                width: 24,
+                color: kBlue,
+              ),
+              label: 'profile',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
