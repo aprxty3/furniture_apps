ee6d66f6e1968c0afaf7c8b359dfd0c98471b126
aprxrt3 <ajiprxty3@gmail.com>
2022-01-30 20:53:34 +0700

    add bottomnavbar

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -39,6 +39,55 @@ class DetailPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: kWhiteGrey,
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(20),
+        width: double.infinity,
+        height: 96,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: kWhite,
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: 56,
+              height: 56,
+              decoration: BoxDecoration(
+                color: kWhiteGrey,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                child: Image.asset(
+                  'assets/icon_shopping_cart.png',
+                  width: 32,
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            Expanded(
+              child: Container(
+                width: 300,
+                height: 56,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: kBlack,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Book Now',
+                    style: booknowStyle,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Stack(
         children: [
           Image.asset(
