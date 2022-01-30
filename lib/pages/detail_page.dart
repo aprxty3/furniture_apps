73b0f700b14a5968fe4745883a0ca6e6b71a262a
aprxrt3 <ajiprxty3@gmail.com>
2022-01-30 22:10:49 +0700

    add animated change color

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:furniture_apps/theme.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  Color indicatorColor = kBlackAccent;
+  double indicatorMargin = 5;
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +45,99 @@ class DetailPage extends StatelessWidget {
       );
     }
 
+    Widget mainContent() {
+      return SizedBox.expand(
+        child: DraggableScrollableSheet(
+          initialChildSize: 0.32,
+          minChildSize: 0.32,
+          maxChildSize: 0.9,
+          builder: (BuildContext build, ScrollController scrollController) {
+            return Container(
+              padding: const EdgeInsets.only(top: 16, left: 24, right: 24),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: kWhite,
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(40),
+                ),
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 10),
+                        width: 30,
+                        height: 4,
+                        decoration: BoxDecoration(
+                          color: kLineDark,
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Poang Chair',
+                          style: detailStyle1,
+                        ),
+                        Text(
+                          '\$219',
+                          style: detailStyle2,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Text(
+                      'IKOE',
+                      style: detailStyle3,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Container(
+                      height: 50,
+                      child: Stack(
+                        alignment: Alignment.centerLeft,
+                        children: [
+                          Row(
+                            children: [
+                              colorIndicator(kBlackAccent, 0),
+                              colorIndicator(kYellow, 1),
+                              colorIndicator(kGrey, 2),
+                              colorIndicator(kBlack, 3),
+                              colorIndicator(kWhiteGrey, 4),
+                            ],
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: indicatorMargin),
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: indicatorColor,
+                              border: Border.all(
+                                width: 3,
+                                color: kWhite,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kWhiteGrey,
       bottomNavigationBar: Container(
@@ -63,11 +164,9 @@ class DetailPage extends StatelessWidget {
                 ),
               ),
             ),
-            const SizedBox(
-              width: 16,
-            ),
             Expanded(
               child: Container(
+                margin: const EdgeInsets.only(left: 16),
                 width: 300,
                 height: 56,
                 child: TextButton(
@@ -94,12 +193,21 @@ class DetailPage extends StatelessWidget {
             'assets/top_background.png',
             fit: BoxFit.cover,
           ),
-          ListView(
-            children: [
-              header(),
-            ],
-          )
+          header(),
+          mainContent(),
         ],
+      ),
+    );
+  }
+
+  Widget colorIndicator(Color color, int index) {
+    return Container(
+      margin: const EdgeInsets.only(right: 12),
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
       ),
     );
   }
